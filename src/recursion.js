67e24b8fbb8fe0ff77e5161110e9ba7d95558930
David Darley <daviddar232@gmail.com>
2017-11-25 12:23:25 -0800

    Finished #28 Capitalize first letter of words in array

--- src/recursion.js
+++ src/recursion.js
@@ -371,6 +371,14 @@ var capitalizeWords = function(array) {
 // 28. Given an array of strings, capitalize the first letter of each index.
 // capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
 var capitalizeFirst = function(array) {
+  var temp = [];
+  if (array.length) {
+    temp.push(array[0].charAt(0).toUpperCase() + array[0].slice(1));
+    console.log('maybe',temp);
+    return temp.concat(capitalizeFirst(array.slice(1)));
+  }
+  console.log(temp);
+  return temp;
 };
 
 // 29. Return the sum of all even numbers in an object containing nested objects.
